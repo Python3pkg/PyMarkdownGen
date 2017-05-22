e83cf7ee0e31993c4c29d4c214bbafd7c7f99cbb
Python3pkg <raliclo@gmail.com>
2017-05-22 10:24:32 -0700

    Convert to Python3

--- docs/conf.py
+++ docs/conf.py
@@ -52,8 +52,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PyMarkdownGen'
-copyright = u'2015, Lukas Woodtli'
+project = 'PyMarkdownGen'
+copyright = '2015, Lukas Woodtli'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -205,8 +205,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'PyMarkdownGen.tex', u'PyMarkdownGen Documentation',
-   u'Lukas Woodtli', 'manual'),
+  ('index', 'PyMarkdownGen.tex', 'PyMarkdownGen Documentation',
+   'Lukas Woodtli', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -235,8 +235,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pymarkdowngen', u'PyMarkdownGen Documentation',
-     [u'Lukas Woodtli'], 1)
+    ('index', 'pymarkdowngen', 'PyMarkdownGen Documentation',
+     ['Lukas Woodtli'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -249,8 +249,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'PyMarkdownGen', u'PyMarkdownGen Documentation',
-   u'Lukas Woodtli', 'PyMarkdownGen', 'One line description of project.',
+  ('index', 'PyMarkdownGen', 'PyMarkdownGen Documentation',
+   'Lukas Woodtli', 'PyMarkdownGen', 'One line description of project.',
    'Miscellaneous'),
 ]
 
